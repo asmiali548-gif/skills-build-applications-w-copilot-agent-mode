8fe7bd14640598e5e6b3762a5e6305d222bac1ed
asmiali548-gif <asmiali548@gmail.com>
2026-05-14 14:56:03 +0000

    Fix Step 3: Add settings.py and populate_db command

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -1,11 +1,7 @@
 from pathlib import Path
-
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-SECRET_KEY = 'django-insecure-your-secret-key-here'
-
+SECRET_KEY = 'django-insecure-key'
 DEBUG = True
-
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
@@ -30,23 +26,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'octofit_tracker.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
 WSGI_APPLICATION = 'octofit_tracker.wsgi.application'
 
 DATABASES = {
@@ -60,6 +39,5 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
